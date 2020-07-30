7b614944e8072ba7f953c3ec685e1e07a9d85432
DrewRust <drewrust1@gmail.com>
2020-07-30 14:02:03 -0400

    Cleaned up comments, added teachers solution

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,31 +1,31 @@
-# imports
+#### imports
 import os
 import sqlite3
 import psycopg2
 import pandas as pd
 from dotenv import load_dotenv
 
-# loading .env credentials
+#### loading .env credentials
 load_dotenv()
 
-# credentials 
+#### credentials 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 
-# establish connection to sqlite
+#### establish connection to sqlite
 conn = psycopg2.connect(dbname=DB_NAME, 
                         user=DB_USER,
                         password=DB_PASS, 
                         host=DB_HOST)
-# create cursor object to connection
+#### create cursor object to connection
 postGres_cursor2 = conn.cursor()
 
-# read in titanic csv
+#### read in titanic csv
 read_titanic = pd.read_csv (r'titanic.csv', nrows=20)
 
-# create empty SQL table if it doesn't already exist
+#### create empty SQL table if it doesn't already exist
 create_titanic_table_query = '''
 CREATE TABLE IF NOT EXISTS titanic (
     id SERIAL PRIMARY KEY,
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS titanic (
 	parents_children_aboard INT
 )
 '''
-# point table columns to cursor object
+#### point table columns to cursor object
 postGres_cursor2.execute(create_titanic_table_query)
-# commit to table plus or postgreSql
+#### commit to table plus or postgreSql
 conn.commit()
-# function to add individual passengers from csv inputs
+#### function to add individual passengers from csv inputs
 def insertPassengerIntoTable(id, survived, name, sex, age, fare, pclass, siblings_spouses_aboard, parents_children_aboard):
     passenger = (id, survived, name, sex, age, fare, pclass, siblings_spouses_aboard, parents_children_aboard)
 
@@ -54,7 +54,8 @@ def insertPassengerIntoTable(id, survived, name, sex, age, fare, pclass, sibling
     '''
     postGres_cursor2.execute(insert_query)
 
-# x is to create id's for each passenger
+#### Calling the above function below for as many rows that are in the .csv file
+#### x is to create id's for each passenger
 x = 1
 for ind in read_titanic.index:
     insertPassengerIntoTable(
@@ -71,10 +72,10 @@ for ind in read_titanic.index:
     x += 1
 conn.commit()
      
-# testing out the function it works!
-# insertVaribleIntoTable(2, 1, "Mr. George Smith", "female", 11, 56.5, 3, 6, 5)
+#### testing out the function it works!
+#### insertVaribleIntoTable(2, 1, "Mr. George Smith", "female", 11, 56.5, 3, 6, 5)
 
-# close cursor and connection
+#### close cursor and connection
 postGres_cursor2.close()
 conn.close()
 
